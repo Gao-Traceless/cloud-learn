6a469219ec082f2c607a3e668ef49cfa451e4288
Gao-XY <artistgao@gmail.com>
2023-04-01 14:45:43 +0800

    20230401订单系统搭建练习

--- cloud2023/cloud2023/cloud-provider-payment8001/src/main/java/com/gao/springcloud/controller/PaymentController.java
+++ cloud2023/cloud2023/cloud-provider-payment8001/src/main/java/com/gao/springcloud/controller/PaymentController.java
@@ -5,10 +5,7 @@ import com.gao.springcloud.entities.CommonResult;
 import com.gao.springcloud.entities.Payment;
 import com.gao.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -21,14 +18,14 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping("/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult create(@RequestBody Payment payment){
 
         int result = paymentService.create(payment);
         log.info("********插入结果: " + result);
         if (result > 0){
             return new CommonResult(200, "插入数据库成功", result);
         }else {
-            return new CommonResult(444, "插入数据库失败", null);
+            return new CommonResult(444, "插入数据库失败" , null);
         }
 
 
