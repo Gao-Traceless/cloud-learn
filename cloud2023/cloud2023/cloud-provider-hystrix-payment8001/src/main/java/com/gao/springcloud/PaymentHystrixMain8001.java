da7be00861c05ce7d54aa40f7344091880a32b67
Gao-XY <artistgao@gmail.com>
2023-04-09 23:35:11 +0800

    【20230409】 Hystrix的服务熔断学习+监控学习

--- cloud2023/cloud2023/cloud-provider-hystrix-payment8001/src/main/java/com/gao/springcloud/PaymentHystrixMain8001.java
+++ cloud2023/cloud2023/cloud-provider-hystrix-payment8001/src/main/java/com/gao/springcloud/PaymentHystrixMain8001.java
@@ -1,9 +1,12 @@
 package com.gao.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,6 +15,22 @@ public class PaymentHystrixMain8001 {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+
+
+    /**
+     * 此配置是为了服务监控而配置的，与服务容错本身无关，SpringCloud升级后的坑
+     * ServetRegistrationBean因为SpringBoot的默认路径不是“/hystrix.stream”，
+     * 只要在自己的项目里配置上下面的Servlet就可以了
+     */
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
--- cloud2023/cloud2023/cloud-provider-hystrix-payment8001/src/main/java/com/gao/springcloud/controller/PaymentController.java
+++ cloud2023/cloud2023/cloud-provider-hystrix-payment8001/src/main/java/com/gao/springcloud/controller/PaymentController.java
@@ -19,6 +19,7 @@ public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
+    //=========================== 服务降级 ====================================
     @GetMapping("/payment/hystrix/ok/{id}")
     public String paymentInfo_OK(@PathVariable("id") Integer id){
         String result = paymentService.paymentInfo_OK(id);
@@ -30,6 +31,15 @@ public class PaymentController {
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
         String result = paymentService.paymentInfo_TimeOut(id);
         log.info("*******result: " + result);
+        return result;
+    }
+
+
+    //=========================== 服务熔断 ====================================
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id){
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("*******result:"+result);
         return result;
     }
 
