7755b909889f222d5e101faad47373af51fab0f8
Gao-XY <artistgao@gmail.com>
2023-04-21 23:41:20 +0800

    【20230409】 nacos注册中心，配置中心和Sentinel学习

---
+++ cloud2023/cloud2023/cloudalibaba-sentinel-service8401/src/main/java/com/gao/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,26 @@
+package com.gao.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA() {
+
+//        try {TimeUnit.MILLISECONDS.sleep(800);} catch (InterruptedException e) {e.printStackTrace();}
+
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+
+        return "------testB";
+    }
+
+
+}
